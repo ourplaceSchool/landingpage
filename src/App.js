7c4b5654d87ea2d0026d41d65df1e6787712b9ce
Daniel Costa <dancosta.id@gmail.com>
2022-07-02 14:34:28 -0300

    routers are functional

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import GlobalStyle from './globalStyles';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer'
-// import Home from './Home';
-// import QuemSomos from './QuemSomos';
+import Home from './Home';
+import QuemSomos from './QuemSomos';
 import FaleConosco from './FaleConosco';
-// import Metodologia from './Metodologia';
-// import Galeria from './Galeria';
+import Metodologia from './Metodologia';
+import Galeria from './Galeria';
 
 const theme = {
   colors: {
@@ -22,9 +23,17 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <NavBar />
-      <FaleConosco />
-      <Footer />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="sobre" element={<QuemSomos />} />
+          <Route path="metodologia" element={<Metodologia />} />
+          <Route path="galeria" element={<Galeria />} />
+          <Route path="contato" element={<FaleConosco />} />
+        </Routes>
+        <Footer />
+      </Router>
     </ThemeProvider>
   );
 }
