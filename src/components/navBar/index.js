1496b75528de4aa7a628a0456a3a452a52a5d0f7
Daniel Costa <dancosta.id@gmail.com>
2022-07-10 17:14:31 -0300

    Good work on the NavBar. Different navbar for different screen sizes. Still need to make it work widthout re-loading the page. Also missing the logic for the dropdown menu

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { HiOutlineMenuAlt3 } from 'react-icons/hi';
 import { NavLink } from "react-router-dom";
+import { device } from '../../device';
 import { 
   Container,
   Logo,
   Wrapper,
   Navegation,
-  Button,
-  BurgerLineStyle
+  Button
 } from './styles'
 
 export default function NavBar() {
@@ -22,63 +22,80 @@ export default function NavBar() {
     textDecoration: 'none',
   }
 
-  return (
-    <Container>
+
+  // useEffect(() => {
+    
+  // })
+
+
+  if(window.innerWidth < 769){
+    console.log('Tamanho da Janela', window.innerWidth)
+    return(
+      <Container>
+        
       <Logo />
       <HiOutlineMenuAlt3 style={{
         color: 'white',
         height: '25px',
         width: '25px',
       }}/>
-      {/*
-      <Wrapper>
+      </Container>
+    )
+  } else {
+    console.log('Tamanho da Janela', window.innerWidth)
+    return (
+      <Container>
+        <Logo />
+        <Wrapper>
 
-        <Navegation>
-          <NavLink
-            to="/"
-            style={({ isActive }) =>
-              isActive ? activeStyle : nonActive
-            }
-          > Home </NavLink>
-        </Navegation>
-
-        <Navegation>
-          <NavLink
-              to="/metodologia"
+          <Navegation>
+            <NavLink
+              to="/"
               style={({ isActive }) =>
                 isActive ? activeStyle : nonActive
               }
-            > Metodologia </NavLink>
-        </Navegation>
+            > Home </NavLink>
+          </Navegation>
 
-        <Navegation>
-          <NavLink
-              to="/sobre"
-              style={({ isActive }) =>
-                isActive ? activeStyle : nonActive
-              }
-          > Quem Somos </NavLink>
-        </Navegation>
+          <Navegation>
+            <NavLink
+                to="/metodologia"
+                style={({ isActive }) =>
+                  isActive ? activeStyle : nonActive
+                }
+              > Metodologia </NavLink>
+          </Navegation>
 
-        <Navegation>
-          <NavLink
-              to="/contato"
-              style={({ isActive }) =>
-                isActive ? activeStyle : nonActive
-              }
-          > Fale Conosco </NavLink>
-        </Navegation>
+          <Navegation>
+            <NavLink
+                to="/sobre"
+                style={({ isActive }) =>
+                  isActive ? activeStyle : nonActive
+                }
+            > Quem Somos </NavLink>
+          </Navegation>
 
-        <Navegation>
-          <NavLink
-              to="/galeria"
-              style={({ isActive }) =>
-                isActive ? activeStyle : nonActive
-              }
-          > Galeria </NavLink>
-        </Navegation>
-      </Wrapper>
-      <Button>MATRICULE-SE</Button> */}
-    </Container>
-  )
+          <Navegation>
+            <NavLink
+                to="/contato"
+                style={({ isActive }) =>
+                  isActive ? activeStyle : nonActive
+                }
+            > Fale Conosco </NavLink>
+          </Navegation>
+
+          <Navegation>
+            <NavLink
+                to="/galeria"
+                style={({ isActive }) =>
+                  isActive ? activeStyle : nonActive
+                }
+            > Galeria </NavLink>
+          </Navegation>
+        </Wrapper>
+        <Button>MATRICULE-SE</Button>
+      </Container>
+    )
+  }
+
 }
